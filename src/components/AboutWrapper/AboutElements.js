2e89d61fd4b9595110946962db53ff2a68f95b11
capricornus44 <antonzavalniuk@gmail.com>
2021-06-20 15:03:10 +0200

    updated background color

--- src/components/AboutWrapper/AboutElements.js
+++ src/components/AboutWrapper/AboutElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const AboutSection = styled.div`
   height: 100vh;
-  background-color: #ebebeb;
+  background-color: #ffffff;
 `
 export const AboutContainer = styled.div`
   width: 1170px;
